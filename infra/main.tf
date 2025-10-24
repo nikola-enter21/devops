1d918a6d725bbb72c6a41f4c58c851311cda26b1
nikola-enter21 <109946282+nikola-enter21@users.noreply.github.com>
2025-10-24 23:08:54 +0300

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -95,7 +95,7 @@ resource "google_container_node_pool" "primary_nodes" {
   name       = "${var.name_prefix}-nodepool"
   location   = var.region
   cluster    = google_container_cluster.primary.name
-  node_count = 2
+  node_count = 1
 
   node_config {
     machine_type = "e2-medium"
