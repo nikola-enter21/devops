36c11e329cb167227ab8d39a3d2348d4444ce22d
nikola-enter21 <109946282+nikola-enter21@users.noreply.github.com>
2025-10-26 17:30:23 +0200

    Trigger infra

--- infra/data.tf
+++ infra/data.tf
@@ -1,3 +1,7 @@
 data "google_compute_default_service_account" "default" {
   project = var.project_id
 }
+
+output "default_compute_service_account_email" {
+  value = data.google_compute_default_service_account.default.email
+}
