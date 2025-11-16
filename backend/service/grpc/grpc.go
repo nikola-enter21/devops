83ddc6536105274d6f6d2585cdde169157c7d5ef
nikola-enter21 <109946282+nikola-enter21@users.noreply.github.com>
2025-11-16 20:10:46 +0200

    Remove info logs from rpcs

--- backend/service/grpc/grpc.go
+++ backend/service/grpc/grpc.go
@@ -10,21 +10,18 @@ import (
 )
 
 func (s *Server) Healthz(ctx context.Context, _ *user.HealthzRequest) (*user.HealthzResponse, error) {
-	log.Infow("health check")
 	return &user.HealthzResponse{
 		Status: "ok",
 	}, nil
 }
 
 func (s *Server) Login(ctx context.Context, req *user.LoginRequest) (*user.LoginResponse, error) {
-	log.Infow("login attempt", "username", req.Username)
 	return &user.LoginResponse{
-		Token: "Login successful2",
+		Token: "Login successful",
 	}, nil
 }
 
 func (s *Server) Register(ctx context.Context, req *user.RegisterRequest) (*user.RegisterResponse, error) {
-	log.Infow("user registration", "username", req.Username, "email", req.Email)
 	return &user.RegisterResponse{
 		Message: "User registered",
 	}, nil
@@ -61,13 +58,6 @@ func (s *Server) CheckDatabase(ctx context.Context, _ *user.CheckDatabaseRequest
 			DbStatus: "unreachable: " + err.Error(),
 		}, nil
 	}
-
-	log.Infow("database connection successful",
-		"host", host,
-		"port", port,
-		"user", usern,
-		"dbname", dbname,
-	)
 
 	return &user.CheckDatabaseResponse{
 		DbStatus: "ok",
